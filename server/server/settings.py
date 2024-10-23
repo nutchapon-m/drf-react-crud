8c9e297e70cbab5cc6b0860229d83859867e39fb
nutchapon-m <nutchapon.met1002@gmail.com>
2024-10-23 12:09:25 +0700

    update config server

--- server/server/settings.py
+++ server/server/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'admin1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
