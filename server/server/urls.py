09068aa4a1c36e2bae6df6ee87d7467cda9607e8
nutchapon <nutchapon.m@swiftdynamics.co.th>
2024-11-04 20:10:15 +0700

    test

--- server/server/urls.py
+++ server/server/urls.py
@@ -7,5 +7,6 @@ router.register(r'book', views.BookView)
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
     path('', include(router.urls)),
 ]
